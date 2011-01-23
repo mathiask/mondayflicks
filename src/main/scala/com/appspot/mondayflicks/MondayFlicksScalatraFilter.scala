e742e3c3309262e5d0d7ebb7a4be5db6300f7188
Mathias Kegelmann <mathias.kegelmann@gmail.com>
2011-01-23 15:18:25 +0100

    pulled out Style

--- src/main/scala/com/appspot/mondayflicks/MondayFlicksScalatraFilter.scala
+++ src/main/scala/com/appspot/mondayflicks/MondayFlicksScalatraFilter.scala
@@ -3,70 +3,18 @@ package com.appspot.mondayflicks
 import util._
 
 import scala.xml._
-import scala.xml.dtd.{DocType, PublicID}
 import org.scalatra._
 
-import java.io.StringWriter
 import javax.servlet.FilterConfig
 
 import com.google.appengine.api.users.User
 
-class MondayFlicksScalatraFilter extends ScalatraFilter 
-with UserSupport with CalendarAccessSupport with TweeterSupport with Scripts 
+class MondayFlicksScalatraFilter extends ScalatraFilter
+with Scripts with UserSupport
+with CalendarAccessSupport with TweeterSupport
 with Logging {
 
-  object Template {
-    private val style =
-      """
-      |body {
-      |  font-family: Trebuchet MS, sans-serif;
-      |  padding-left: 30px; padding-right: 30px; padding-top: 30px;
-      |  min-width: 50ex; max-width: 100ex;
-      |  margin-left: auto; margin-right: auto;
-      |  background-color: white;
-      |}
-      |#main {
-      |  padding: 2ex;
-      |  background-color: #eeeeee;
-      |  border: 2px solid #003b6b;
-      |  border-radius: 8px; -moz-border-radius: 8px; -webkit-border-radius: 8px;
-      |}
-      |#content {
-      |  min-height: 180px;
-      |}
-      |h1 { color: #005580; margin-top: 0px; }
-      |h3 { margin: 0px; color: #003b6b; }
-      |span.item { margin-right: 1em; }
-      |a { color: #404040; }
-      |a:hover, .editable-highlighted { background-color: #8ECAE8; }
-      |input, textarea { border-radius: 4px; -moz-border-radius: 4px; -webkit-border-radius: 4px; }
-      |div.user, span.tiny { color: gray; font-size: small; font-style: italic; font-weight: lighter; }
-      |div.comment {
-      |  padding: 4px;
-      |  border: 2px solid #005580;
-      |  border-radius: 4px; -moz-border-radius: 4px; -webkit-border-radius: 4px;
-      |  width: 40em; margin-bottom: 2ex;
-      |}
-      |input.delete { float: right; }
-      |div.sidebar {
-      |  float: right;
-      |  margin: 1ex;
-      |  padding: 1ex;
-      |  background-color: #e0e0e0;
-      |  border: 1px solid #003b6b;
-      |  border-radius: 8px; -moz-border-radius: 8px; -webkit-border-radius: 8px;
-      |  width: 33%;
-      |}
-      |div.popup { position: absolute; top: 2ex; left: -1.5em; display: none; z-index: 1; }
-      |div.error { font-size: large; color: red; }
-      |img.icon { width: 16px; height: 16px; vertical-align: middle; margin-right: 0.5em;}
-      |div.appengine, a.login { float: right; margin-left: 1ex; }
-      """.stripMargin
-
-    private val doctype = DocType("html", PublicID("-//W3C//DTD XHTML 1.0 Strict//EN",
-                                                   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"), 
-                                  Nil)
-
+  object Template extends Style {
     def page(title:String,
              content:NodeSeq,
              sidebar: Option[NodeSeq] = None,
@@ -103,10 +51,8 @@ with Logging {
             </div>
           </body>
         </html>
-      val writer = new StringWriter
-      XML.write(writer, doc, "UTF-8", true, doctype)
       contentType = "text/html"
-      writer.toString
+      asXHTMLWithDocType(doc)
     }
   }
 
@@ -126,11 +72,11 @@ with Logging {
         Template.page("Illegal Access",
                       <div class="error">The film or comment does not (or no longer) exists.</div>
                       <div><a href={startPage}>Restart</a></div>)
-      case t => 
+      case t =>
         error(t)
         Template.page("Error",
                       <div class="error">Internal server error.</div>
-                      <div><a href={startPage}>Restart</a></div>)      
+                      <div><a href={startPage}>Restart</a></div>)
     }
   }
 
@@ -295,10 +241,10 @@ with Logging {
               $(function(){
                 $('#schedule').hide();
                 $('#scheduledFor').datepicker({
-                  firstDay: 1, 
+                  firstDay: 1,
                   dateFormat: 'yy-mm-dd',
                   onSelect: function(){ $('#schedule').click(); }
-                }); 
+                });
               });
             </xml:unparsed></script>
           } else {
