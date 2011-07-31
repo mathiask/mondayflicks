539ac3536c133118a30ffbd26d7567008302e8fc
Mathias Kegelmann <mathias.kegelmann@gmail.com>
2011-07-31 11:40:21 +0200

    ensime fixes

--- project/build/MondayFlicksProject.scala
+++ project/build/MondayFlicksProject.scala
@@ -13,7 +13,7 @@ class MondayFlicksProject(info: ProjectInfo) extends AppengineProject(info) with
   // val googleApiClientGoogleapis = "com.google.api.client" % "google-api-client-googleapis" % "1.4.1-beta"
   // val googleApiClientExtendsions = "com.google.api.client" % "google-api-client-extensions" % "1.4.1-beta"
 
-  val scalatraTest = "org.scalatra" %% "scalatra-scalatest" % scalatraVersion % "test"
+  val scalatraScalatest = "org.scalatra" %% "scalatra-scalatest" % scalatraVersion % "test"
   val scalatest = "org.scalatest" % "scalatest_2.9.0" % "1.6.1" % "test"
 
   val sonatypeNexusSnapshots = "Sonatype Nexus Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
